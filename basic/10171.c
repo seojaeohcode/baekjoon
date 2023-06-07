d133eaa731ec53c6ee55d7bc87f844c4c12cf6e8
seojaeoh <seojaeohcoder@gmail.com>
2023-06-07 13:44:56 +0900

    10171_solved

---
+++ basic/10171.c
@@ -0,0 +1,12 @@
+
+//특문 출력시 '\'붙이면 됨
+
+#include <stdio.h>
+
+int main() {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+    return 0;
+}
